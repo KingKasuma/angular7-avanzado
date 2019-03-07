d834b29f3d9269bf31a380c805686caf0b7a1f37
Mike <lanselot777hailander@gmail.com>
2019-03-07 12:14:47 -0400

    localStorage

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit{
   title = 'Contact';
+  emailContacto: string;
 
   ngOnInit(){
     console.log('contact.component cargado');
+  }
+  guardarEmail(){
+    localStorage.setItem('emailContacto', this.emailContacto);
+    
   }
 
 }
